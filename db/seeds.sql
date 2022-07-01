da648e879e376391404379a436709a09b50c1967
Jeremiah Brockett <98862284+Jeremiah-81@users.noreply.github.com>
2022-07-01 12:04:23 -0700

    Updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,23 @@
-INSERT INTO departments (id, dept_name)
-VALUES (001, "name", "departments");
+INSERT INTO departments (dept_name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (001, "title")
-       (002, "salary")
-       (003, "department_id");
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1),
+       ("Lead Engineer", 15000, 2),
+       ("Software Engineer", 12000, 2),
+       ("Account Manager", 16000, 2),
+       ("Accountant", 125000, 2),
+       ("Legal Team Lead", 25000, 2),
+       ("Lawyer", 19000, 2);
 
 INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES (001, "first_name")
-       (002, "last_name")
-       (003,  "role_id")
-       (004,  "manager_id")
+VALUES ("Samus", "Aran", 1, 1),
+       ("Geordi", "LaForge", 2, null),
+       ("Kaylee", "Frye", 2, 2),
+       ("James", "Bond", 2, null),
+       ("Bruce", "Wayne", 2, 2),
+       ("Tony", "Stark", 2, null),
+       ("Fletcher", "Reede", 2, 2),;
